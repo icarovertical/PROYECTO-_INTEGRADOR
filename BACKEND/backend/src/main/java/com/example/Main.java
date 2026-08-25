7c57f0a8d863fe9e209497957c28ddc85386c1e2
icarovertical <icarovertical@gmail.com>
2026-08-24 22:39:58 -0500

    Taller 1 y 2

---
+++ BACKEND/backend/src/main/java/com/example/Main.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Product producto1 = new Product();
+       
+
+
+        System.out.println("Producto1: "+ producto1);
+      
+    }
+    
+} 
---
+++ BACKEND/backend/src/main/java/com/example/Product.java
@@ -0,0 +1,59 @@
+package com.example;
+
+public class Product {
+    // Atributos
+    private String id;
+    private String name;
+    private Double price;
+    private int stock;
+
+    // Constructor Parametrizado
+    public Product (String id, String name, Double price, int stock) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    //Getter y Setter
+     public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+   
+   
+    //
+    @Override
+    public String toString() {
+        return "ID: "+ id +"Name:" + name +"Price: " + price +"Stock: " + stock;
+    }
+
+   
+}
