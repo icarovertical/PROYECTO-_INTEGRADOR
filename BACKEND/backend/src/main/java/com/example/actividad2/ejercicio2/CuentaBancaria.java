4dac86d4cc3b8c7ac1c7c689632ff730dd9ac798
icarovertical <icarovertical@gmail.com>
2026-08-30 23:09:58 -0500

    Prueba2
    prueba 2 Git

--- BACKEND/backend/src/main/java/com/example/actividad2/ejercicio2/CuentaBancaria.java
+++ BACKEND/backend/src/main/java/com/example/actividad2/ejercicio2/CuentaBancaria.java
@@ -42,7 +42,7 @@ public class CuentaBancaria {
             saldo -= cantidad;
         }
     }
-//Prueba de GitHUB
+//Prueba de GitHUB 2
     public void mostrarDetalles() {
         System.out.println("Titular: [" + titular + "] | Saldo: [" + saldo + "]");
     }
