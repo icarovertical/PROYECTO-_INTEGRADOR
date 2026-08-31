70a13eb9b5d4d9567704dc05cdc7f534f77c355a
icarovertical <icarovertical@gmail.com>
2026-08-30 23:27:29 -0500

    Prueba Final 7

--- BACKEND/backend/src/main/java/com/example/actividad2/ejercicio2/CuentaBancaria.java
+++ BACKEND/backend/src/main/java/com/example/actividad2/ejercicio2/CuentaBancaria.java
@@ -42,7 +42,7 @@ public class CuentaBancaria {
             saldo -= cantidad;
         }
     }
-// Ultimos cambios
+// Ultimos cambios 7
     public void mostrarDetalles() {
         System.out.println("Titular: [" + titular + "] | Saldo: [" + saldo + "]");
     }
--- BACKEND/backend/src/main/java/com/example/actividad2/ejercicio3/Estudiante.java
+++ BACKEND/backend/src/main/java/com/example/actividad2/ejercicio3/Estudiante.java
@@ -52,7 +52,7 @@ public class Estudiante {
     public boolean haAprobado() {
         return promedio >= 3.0;
     }
-// Ultimos cambios
+// Ultimos cambios 7
     public void mostrarInfo() {
         System.out.println("Nombre: " + nombre + "edad: " + edad + "Promedio: " + promedio);
         if (haAprobado()) {
